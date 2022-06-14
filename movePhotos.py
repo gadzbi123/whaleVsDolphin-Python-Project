5f263dc2220ff6f6eaa52d4876cb2246dd61d65e
Gadzbi <crg1234@gmail.com>
2022-06-14 20:04:29 +0200

    complete projet with no gui

---
+++ movePhotos.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import shutil as sh
+import numpy as np
+
+
+data = pd.read_csv('train.csv')
+a = data.head(50000)
+
+i = 0
+hw_count = 0
+bd_count = 0
+src_dir = "./happy-whale-and-dolphin/train_images/"
+
+data_to_store = {
+    'Img_source': [],
+    'Animal': [],
+}
+
+for idx, img in enumerate(a.values):
+    if img[1] == 'humpback_whale' and hw_count != 250 and idx > 35000:
+        dest = "./ds/single_test/" + str(i) + '.jpg'
+        sh.copy(src_dir + img[0], dest)
+        data_to_store['Img_source'].append(str(i) + '.jpg')
+        data_to_store['Animal'].append("humpback_whale")
+        hw_count += 1
+        i += 1
+
+    if img[1] == 'bottlenose_dolphin' and bd_count != 250 and idx > 35000:
+        dest = "./ds/single_test/" + str(i) + '.jpg'
+        sh.copy(src_dir + img[0], dest)
+        data_to_store['Img_source'].append(str(i) + '.jpg')
+        data_to_store['Animal'].append("bottlenose_dolphin")
+        bd_count += 1
+        i += 1
+
+
+print("whale " + str(hw_count), ", dolphin " + str(bd_count))
+df = pd.DataFrame(data_to_store)
+df.to_csv("./test.csv", index=False)
---
+++ pythonversion.py
@@ -0,0 +1,5 @@
+import requests
+import sys
+print(sys.path)
+print(sys.executable)
+print(sys.version)
